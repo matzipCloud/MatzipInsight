055fd79a60bb0f5f6023fbbe3bb13332da0fa2ea
la0503ab <la0503ab@naver.com>
2024-07-26 17:51:15 +0900

    🔨 페이지 벗어날 시 파일삭제 기능 추가

--- app/views.py
+++ app/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from .utils import save_reviews_to_csv
+import os
 import time
-import json
 import requests
+
 try:
     import pandas as pd
 except ImportError as e:
     print("Pandas could not be imported:", e)
 
+
 # Create your views here.
 
 def home(request):
     return render(request, 'app/home.html')
+
 
 def search_result(request):
     query = request.GET.get('query')  # 사용자로부터 검색어를 가져옴
@@ -40,7 +45,7 @@ def search_result(request):
         response = requests.get(url, headers=headers, params=params)
         response.raise_for_status()  # 요청 실패 시 에러 발생
         data = response.json()
-        
+
         # 검색 결과를 저장할 리스트 초기화
         results = []
 
@@ -55,7 +60,7 @@ def search_result(request):
 
         # 검색 결과를 context에 추가
         context['results'] = results
-        
+
         # 검색 결과를 세션에 저장
         request.session['search_results'] = results
 
@@ -65,7 +70,7 @@ def search_result(request):
 def search_detail(request, id):
     # 세션에서 검색 결과 가져오기
     results = request.session.get('search_results', [])
-    
+
     # id를 문자열로 변환 후 검색
     result = next((item for item in results if item['id'] == str(id)), None)
 
@@ -81,7 +86,7 @@ def search_detail(request, id):
         options.add_argument("--headless=new")
         options.add_argument('user-agent=' + user_agent)
         driver = webdriver.Chrome(options=options)
-        
+
         res = driver.get(f'https://m.place.naver.com/restaurant/{id}/review/visitor')
         driver.implicitly_wait(20)
 
@@ -90,10 +95,11 @@ def search_detail(request, id):
         driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
 
         # 최대 5개의 리뷰를 가져오기 위한 루프
-        while len(reviews) < 5:
+        while len(reviews) < 1000:
             try:
-                driver.find_element(By.XPATH, '//*[@id="app-root"]/div/div/div/div[6]/div[2]/div[3]/div[2]/div/a').click()
-                time.sleep(0.5)
+                driver.find_element(By.XPATH,
+                                    '//*[@id="app-root"]/div/div/div/div[6]/div[2]/div[3]/div[2]/div/a').click()
+                time.sleep(0.05)
             except Exception as e:
                 print('finish')
                 break
@@ -104,7 +110,7 @@ def search_detail(request, id):
             review_elements = bs.select('li.owAeM')
 
             for review in review_elements:
-                if len(reviews) >= 5:
+                if len(reviews) >= 1000:
                     break
                 # 개별 콘텐츠 선택하여 읽어들이는 부분
                 nickname = review.select_one('span.P9EZi')
@@ -121,11 +127,24 @@ def search_detail(request, id):
                 reviews.append({'nickname': nickname, 'content': content, 'date': date, 'revisit': revisit})
                 time.sleep(0.05)
 
-        context['reviews'] = reviews
-
+        context['reviews'] = reviews[:5]
+        #파일로 저장
+        save_reviews_to_csv(reviews, id)
     except Exception as e:
         print(e)
     finally:
         driver.quit()
 
     return render(request, 'app/search_detail.html', context)
+
+
+def delete_review_file(request, id):
+
+    filename = f'reviews_{id}.csv'
+    filepath = os.path.join('data/', filename)
+
+    if os.path.exists(filepath):
+        os.remove(filepath)
+        return JsonResponse({'status': 'success'})
+    else:
+        return JsonResponse({'status': 'file not found'}, status=404)
